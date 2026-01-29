863d872f7fa1e43ac1893d14aa97b33ea2e4c158
nepsidehustle <nblecomm@outlook.com>
2026-01-28 21:39:54 -0500

    radio fix2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  // Force Cloudflare back to Server mode
   output: 'server',
   adapter: cloudflare(),
   base: '/',
