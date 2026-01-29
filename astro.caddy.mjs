1b6fa8255d666e80bfc06e170d01e850bf37de13
nepsidehustle <nblecomm@outlook.com>
2026-01-28 20:59:40 -0500

    deploy6

--- astro.caddy.mjs
+++ astro.caddy.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  build: {
+    format: 'directory' // Ensures we get /radio/index.html instead of radio.html
+  },
   base: '/radio',
   trailingSlash: 'always',
 });
