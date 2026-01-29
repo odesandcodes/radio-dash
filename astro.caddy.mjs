dcbceeea8ccecd5f3a236be7d370656a068c38c5
nepsidehustle <nblecomm@outlook.com>
2026-01-28 21:07:49 -0500

    deploy8

--- astro.caddy.mjs
+++ astro.caddy.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node"; // The "Dummy" adapter
+import node from "@astrojs/node";
 
 export default defineConfig({
-  output: 'static', // Still static!
+  output: 'static', // This is the key
   adapter: node({
     mode: "standalone"
   }),
+  // This forces Astro to put everything in /dist instead of /dist/client
+  build: {
+    format: 'directory',
+    client: './' 
+  },
   base: '/radio',
   trailingSlash: 'always',
 });
