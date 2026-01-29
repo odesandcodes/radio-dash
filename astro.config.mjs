b067ff08f72f58f270e34edbcd64fec0cfa431bb
nepsidehustle <nblecomm@outlook.com>
2026-01-28 21:43:19 -0500

    radio fix3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  // Force Cloudflare back to Server mode
-  output: 'server',
+  output: 'server', // Cloudflare NEEDS this to run the API routes
   adapter: cloudflare(),
-  base: '/',
+  base: '/', // Cloudflare apps usually run at the root
   trailingSlash: 'ignore',
 });
